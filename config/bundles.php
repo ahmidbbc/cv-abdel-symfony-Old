476a45151128c854f894305f096fe6d028729aa5
Abdel <moi@abdel.com>
2019-07-08 16:21:55 +0200

    Création home

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,16 +1,27 @@
 <?php
 
-
 namespace App\Controller;
 
 
-class HomeController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class HomeController
+ * @package App\Controller
+ */
+class HomeController extends AbstractController
 {
 
+
+    /**
+     * @Route("/")
+     * @return Response
+     */
     public function indexAction(){
-
-
-
+        return $this->render("home/home.html.twig");
     }
 
 }
