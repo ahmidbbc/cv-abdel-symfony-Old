ab968cbb3753535723e5560e6db81c0d6b2070ad
Abdel <moi@abdel.com>
2019-07-08 15:31:33 +0200

    Webpack assets concat

--- webpack-config.js
+++ webpack-config.js
@@ -1,15 +1,19 @@
 const Encore = require('@symfony/webpack-encore');
 
-//dossier contetnant tous les fichiers accessible pour l'App = public/sous-dossier
+//dossier contenant tous les fichiers accessible pour l'App = public/sous-dossier
 Encore  .setOutputPath('public/build')
     .setPublicPath('/build')
     .cleanupOutputBeforeBuild()
     .addEntry('js/app',[
-            'node_modules/jquery/dist/jquery.js',
-            'node_modules/popper.js/dist/umd/popper.js',
-            'node_modules/bootstrap/dist/js/bootstrap.js'
+        './node_modules/jquery/dist/jquery.js',
+        './node_modules/popper.js/dist/umd/popper.js',
+        './node_modules/bootstrap/dist/js/bootstrap.js',
+        './assets/js/app.js'
+    ])
+    .addStyleEntry('css/app', [
+        './node_modules/bootstrap/dist/css/bootstrap.css',
+        './assets/css/app.css'
     ]);
-
 
 
 module.exports = Encore.getWebpackConfig();
